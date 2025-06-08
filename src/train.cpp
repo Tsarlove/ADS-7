f4e976e13441917b274d62143950c5a85078ff76
Tsarlove <olesyolshary@gmail.com>
2025-06-08 22:58:16 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,52 +1,46 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2022 NNTU-CS
 #include "train.h"
-
-Train::Car::Car(bool light_state)
-    : light(light_state), next(nullptr), prev(nullptr) {}
 
 Train::Train() : first(nullptr), countOp(0) {}
 
 void Train::addCar(bool light) {
-    Car* newCar = new Car(light);
-    if (!first) {
-        first = newCar;
-        newCar->next = newCar;
-        newCar->prev = newCar;
-    } else {
-        Car* last = first->prev;
-        last->next = newCar;
-        newCar->prev = last;
-        newCar->next = first;
-        first->prev = newCar;
-    }
+  Car* newCar = new Car{light, nullptr, nullptr};
+  if (!first) {
+    first = newCar;
+    newCar->next = newCar;
+    newCar->prev = newCar;
+  } else {
+    Car* last = first->prev;
+    last->next = newCar;
+    newCar->prev = last;
+    newCar->next = first;
+    first->prev = newCar;
+  }
 }
 
 int Train::getLength() {
-    countOp = 0;
-    Car* current = first;
+  countOp = 0;
+  Car* current = first;
 
-    while (true) {
+  while (true) {
+    countOp++;
+    if (!current->light) {
+      current->light = true;
+      current = current->next;
+    } else {
+      current->light = false;
+      int count = 1;
+      current = current->next;
+      while (!current->light) {
+        count++;
         countOp++;
-        if (!current->light) {
-            current->light = true;
-            current = current->next;
-        } else {
-            current->light = false;
-            int count = 1;
-            current = current->next;
-
-            while (!current->light) {
-                count++;
-                countOp++;
-                current = current->next;
-            }
-
-            return count;
-        }
+        current = current->next;
+      }
+      return count;
     }
+  }
 }
 
-int Train::getOpCount() const {
-    return countOp;
+int Train::getOpCount() {
+  return countOp;
 }
-
